a5f5a55b1510ecd08e5ac9c1a4d5d6e9138f1962
YB <525730167@qq.com>
2019-07-19 10:26:00 +0800

    文件储存到git

--- springboot_jpa_redis/src/main/java/cn/yb/SpringbootJpaRedisApplication.java
+++ springboot_jpa_redis/src/main/java/cn/yb/SpringbootJpaRedisApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootJpaRedisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootJpaRedisApplication.class, args);
+        System.out.println("开始");
+        SpringApplication.run(SpringbootJpaRedisApplication.class, args
+        );
     }
 
 }
